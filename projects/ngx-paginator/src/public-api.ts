299518c88354483f469729707261d8ca9d54deb3
Urs Minder <urs.minder@centeractive.com>
2022-10-16 15:28:11 +0200

    removed auto-generated service

--- projects/ngx-paginator/src/public-api.ts
+++ projects/ngx-paginator/src/public-api.ts
@@ -1,7 +1,5 @@
 /*
  * Public API Surface of ngx-paginator
  */
-
-export * from './lib/ngx-paginator.service';
 export * from './lib/ngx-paginator.component';
 export * from './lib/ngx-paginator.module';
